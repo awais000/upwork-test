e01a1fe6642600de6fb602fbd91947e42aaf7214
awais000 <awaismanzoor00@gmail.com>
2020-12-06 17:14:53 +0500

    added unprotected check-health route for testing

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ import tryangle from './tryangle';
 
 const router = express.Router();
 
+router.use('/check-health',(req, res) => res.send("OK"))
 router.use('/users', authentication);
 router.use('/tryangle', tryangle);
 
